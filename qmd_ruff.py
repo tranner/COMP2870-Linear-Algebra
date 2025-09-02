bc1f785eb7879c0f4ac5d9174eca9be00ae60216
Tom Ranner <T.Ranner@leeds.ac.uk>
2025-09-02 18:34:32 +0200

    more updates

--- qmd_ruff.py
+++ qmd_ruff.py
@@ -46,6 +46,8 @@ def run_ruff_format(file_path):
         print(f"Formatted {file_path} successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error formatting {file_path}: {e}")
+        return 1
+    return 0
 
 
 def run_ruff_check(file_path):
@@ -54,6 +56,8 @@ def run_ruff_check(file_path):
         print(f"Checked {file_path} successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error checking {file_path}: {e}")
+        return 2
+    return 0
 
 
 def run_ruff_sort_imports(file_path):
@@ -64,6 +68,8 @@ def run_ruff_sort_imports(file_path):
         print(f"Sorted imports in {file_path} successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error sorting imports in {file_path}: {e}")
+        return 4
+    return 0
 
 
 def delete_file(file_path):
@@ -85,9 +91,9 @@ def main(infile):
     qmd_to_py(infile, infile_py)
 
     # format and check
-    run_ruff_check(infile_py)
-    run_ruff_format(infile_py)
-    run_ruff_sort_imports(infile_py)
+    r0 = run_ruff_check(infile_py)
+    r1 = run_ruff_format(infile_py)
+    r2 = run_ruff_sort_imports(infile_py)
 
     # convert back to qmd
     py_to_qmd(infile_py, infile)
@@ -95,9 +101,13 @@ def main(infile):
     # delete temporary file
     delete_file(infile_py)
 
+    return r0 + r1 + r2
+
 
 if __name__ == "__main__":
     # read filename
     infiles = sys.argv[1:]
     for infile in infiles:
-        main(infile)
+        r = main(infile)
+        if r != 0:
+            exit(r)
